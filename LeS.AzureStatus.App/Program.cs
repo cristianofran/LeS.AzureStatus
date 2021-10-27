a96a30b4eaae36cf1d166f59c0e39761e0a10d87
Cristiano Leal <cristianofran@hotmail.com>
2021-10-27 11:23:18 -0300

    Criação de um método.

--- LeS.AzureStatus.App/Program.cs
+++ LeS.AzureStatus.App/Program.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System;
 
 namespace LeS.AzureStatus.App
 {
@@ -7,10 +8,16 @@ namespace LeS.AzureStatus.App
     {
         static void Main(string[] args)
         {
+            PesquisarIphoneGoogle();
+        }
+
+        static void PesquisarIphoneGoogle()
+        {
             const string url = "https://www.google.com/";
             const string texto = "Iphone";
 
             using var driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
             driver.Navigate().GoToUrl(url);
 
             var campoPesquisar = driver.FindElement(By.Name("q"));
