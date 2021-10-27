98cc9da4b6e4431e30ede21930a9cc1b89f51f91
Cristiano Francisco Leal da Silva <cristianofran@hotmail.com>
2021-10-27 00:53:02 -0300

    Adição do Selenium

---
+++ LeS.AzureStatus.App/Program.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace LeS.AzureStatus.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string url = "https://www.google.com/";
+            const string texto = "Iphone";
+
+            using var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(url);
+
+            var campoPesquisar = driver.FindElement(By.Name("q"));
+            campoPesquisar.SendKeys(texto);
+
+            var btnPesquisar = driver.FindElement(By.Name("btnK"));
+            btnPesquisar.Click();
+        }
+    }
+}
